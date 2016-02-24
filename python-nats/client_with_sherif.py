e3117de76d0890c917734ab206044e18f907f3a0
Kai Ovesen <kai.ovesen@gmail.com>
2016-02-24 07:54:01 -0800

    Modified client code to now have an working exit condition correct message format. Does not allow for arguments yet.

--- python-nats/client_with_sherif.py
+++ python-nats/client_with_sherif.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import time
 import sys
 
@@ -5,53 +7,71 @@ from nats.client import NatsClient
 
 NATS_URI = "nats://nats:nats@127.0.0.1:4222"
 
-# This example code will not exit cleanly so you will have to kill
-# the process
+
+def get_time():
+    return str(int(round(time.time() * 1000)))
+
+#TODO Program currently exist in the right condition however, occasionally the
+#     following message appears: "[error] Connection closed since ."
 def main():
     try:
+        # parameter declarations
+        user_id = "0"
+        #subscribe_list = ["1", "2"]
+        subscribe_list = ["0"]
+        max_messages = 5
+        # messsage format
+        #     "depart_uid depart_time arrival_uid arrival_time"
+        received_message_list = []
+
+        # start nats process
         nats = NatsClient(uris=NATS_URI)
         nats.start()
         time.sleep(1)
-
         print nats.stat.query()
 
-	# msg = "depart_uid depart_time arrival_uid arrival_time
-	# This works right now. But we want each of these subscriptions
-	# have their own process.
-	#
-	# subprocess psudocode:
-        #	def subscribe_blk(msg):
-	#	    add id and timestamp to msg
-	#	    store msg in memory		
-	#
-        #	sid = nats.subscribe("userID", subscribe_blk)
-	#	
+        # subscribe callback function
+        #     incoming msg format= "user_id time_stamp message_number"
         def subscribe_blk(msg):
-	    #incoming message format: userID time_stamp msg#
-            print "[PUB]: {}".format(msg)
-	    # add id and timestamp to msg
-	    # store msg in memory		
-            for i in range(10):
-	        print s
+            print "[SUB]: received \"{}\"".format(msg)
+            
+            # split funciton was not working correctly so the msg string is being 
+            # stored in a variable
+            received_message_str = str(msg)
+            split_message =  received_message_str.split(" ")
 
+            # extract and store the message number/max messages that will be sent
+            message_number = int(split_message[2])
+            #max_message = int(split_message[3])
+            # extract the message information and add id/timestamp to message
+            final_message = split_message[0] + " " + split_message[1] + " " + user_id + " " + get_time()
+            # store message in memory
+            received_message_list.append(final_message)
 
-        sid = nats.subscribe("id0", subscribe_blk)
-        sid = nats.subscribe("id1", subscribe_blk)
-        sid = nats.subscribe("id2", subscribe_blk)
+            # determine if the last message has come in
+            if message_number >= max_messages:
+                # write recieved_message_list to file
+                print '\nTotal messages recieved by user ' + str(user_id) + ':'
+                for i in received_message_list:
+                    print i
 
+                # stop the nats process to exit the code cleanly
+                time.sleep(1)    # prevent "[error] Connection closed since ." message
+                nats.stop()
 
-	for i in range(1):
-		nats.publish("id0", "user 0: message " + str(i))
-		nats.publish("id1", "user 1: message " + str(i))
-		nats.publish("id2", "user 2: message " + str(i))
-		print "publishing user 0: message " + str(i)
-		print "publishing user 1: message " + str(i)
-		print "publishing user 2: message " + str(i)
-		time.sleep(.2)
+        # subscribe to every user in the subscribe list
+        for ID in subscribe_list:
+            sid = nats.subscribe(ID, subscribe_blk)
+
+        # publish messages and wait between each publish
+        for i in range(max_messages):
+            nats.publish(user_id, user_id + " " + get_time() + " " + str(i + 1))
+            print "[PUB]: publishing message " + str(i + 1)
+            time.sleep(.2)     #TODO make this a random variable with a min of 200 ms
 
     except KeyboardInterrupt, ex:
-        print "ByeBye."
+        print "Keyboard Interrupt Detected. Exiting Program..."
         sys.exit(0)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
