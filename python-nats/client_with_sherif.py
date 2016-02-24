b209f22c0cd3087d7035c9cc11aada630170ae01
Kai Ovesen <kai.ovesen@gmail.com>
2016-02-24 12:12:28 -0800

    client_with_sherif.py: added command line funcitonality, cleaned up comments, updated sub exit condition to support multiple subscriptions, added exit condition if the publishing finishes after the subscriptions have sent all messages.

--- python-nats/client_with_sherif.py
+++ python-nats/client_with_sherif.py
@@ -7,22 +7,32 @@ from nats.client import NatsClient
 
 NATS_URI = "nats://nats:nats@127.0.0.1:4222"
 
-
+# This function is resposible for calculating the current time in UTC
+#TODO this might not be the best method, can be changed.
 def get_time():
     return str(int(round(time.time() * 1000)))
 
-#TODO Program currently exist in the right condition however, occasionally the
-#     following message appears: "[error] Connection closed since ."
+# Main function that drives the main functionality of the client code.
+#
+# Command Line Usage:
+#     ./client_with_sherif.py max_messages this_user_id,sub_id_0,sub_id_1, ... ,sub_id_n
+#
+#TODO change program name
 def main():
     try:
+        if len(sys.argv) != 3:
+           error_str = "ERROR: Command Line Argument Mismatch."
+           error_str += "\n       Usage: "
+           error_str += "./client_with_sherif.py max_messages this_user_id,sub_id_0,sub_id_1, ... ,sub_id_n"
+           sys.exit(error_str)
+
         # parameter declarations
-        user_id = "0"
-        #subscribe_list = ["1", "2"]
-        subscribe_list = ["0"]
-        max_messages = 5
-        # messsage format
-        #     "depart_uid depart_time arrival_uid arrival_time"
-        received_message_list = []
+        max_messages = int(sys.argv[1])          # Max messages this user will publish
+        subscribe_list = sys.argv[2].split(",")  # List of the users that this user is subscribed to
+        user_id = subscribe_list.pop(0)          # This user"s ID
+        received_message_list = []               # List of the messages that have been received
+        completed_user_list = []                 # List of subscribed users who have finished publishing
+        publish_is_complete = False              # Boolean to determine if the publishing is complete
 
         # start nats process
         nats = NatsClient(uris=NATS_URI)
@@ -31,33 +41,38 @@ def main():
         print nats.stat.query()
 
         # subscribe callback function
-        #     incoming msg format= "user_id time_stamp message_number"
         def subscribe_blk(msg):
             print "[SUB]: received \"{}\"".format(msg)
             
-            # split funciton was not working correctly so the msg string is being 
-            # stored in a variable
+            #NOTE: split funciton was not working correctly so the msg string is being 
+            #      stored in a variable
+            #
+            # reveived_message_str= "user_id time_stamp message_number"
             received_message_str = str(msg)
             split_message =  received_message_str.split(" ")
 
-            # extract and store the message number/max messages that will be sent
             message_number = int(split_message[2])
-            #max_message = int(split_message[3])
-            # extract the message information and add id/timestamp to message
+            # final_message = "publish_id sent_time my_id received_time"
             final_message = split_message[0] + " " + split_message[1] + " " + user_id + " " + get_time()
-            # store message in memory
             received_message_list.append(final_message)
 
-            # determine if the last message has come in
+            # determine if the last message for that has been sent
             if message_number >= max_messages:
-                # write recieved_message_list to file
-                print '\nTotal messages recieved by user ' + str(user_id) + ':'
-                for i in received_message_list:
-                    print i
+                completed_user_list.append(split_message[0])
+                print "[SUB]: user " + str(user_id) + " finished publishing"
 
-                # stop the nats process to exit the code cleanly
-                time.sleep(1)    # prevent "[error] Connection closed since ." message
-                nats.stop()
+                # check if all of the subcribed users have completed publishing
+                if completed_user_list == subscribe_list:
+                    #TODO write recieved_message_list to disk
+                    print "\n[SUB]: all messages received:"
+                    for i in received_message_list:
+                        print "    " + i
+
+                    if publish_is_complete:
+                        # stop the nats process to exitsthe code cleanly
+                        print "\nStopping NATS process..."
+                        time.sleep(1)    # prevent "[error] Connection closed since ." message
+                        nats.stop()
 
         # subscribe to every user in the subscribe list
         for ID in subscribe_list:
@@ -68,6 +83,13 @@ def main():
             nats.publish(user_id, user_id + " " + get_time() + " " + str(i + 1))
             print "[PUB]: publishing message " + str(i + 1)
             time.sleep(.2)     #TODO make this a random variable with a min of 200 ms
+
+        publish_is_complete = True
+        if completed_user_list == subscribe_list:
+            # stop the nats process to exitsthe code cleanly
+            print "\nStopping NATS process..."
+            time.sleep(1)    # prevent "[error] Connection closed since ." message
+            nats.stop()
 
     except KeyboardInterrupt, ex:
         print "Keyboard Interrupt Detected. Exiting Program..."
