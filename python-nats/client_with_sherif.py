e7c92a2e3e16cea2c9910f791c82896aef5bc798
Kai Ovesen <kai.ovesen@gmail.com>
2016-02-28 11:23:33 -0800

    Added if statement for if the user is not subscribed to anyone.

--- python-nats/client_with_sherif.py
+++ python-nats/client_with_sherif.py
@@ -5,6 +5,8 @@ import sys
 import random
 
 from nats.client import NatsClient
+from datetime import datetime
+
 
 NATS_URI = "nats://nats:nats@127.0.0.1:4222"
 #NATS_URI = "nats://nats:nats@146.148.76.9:4222"
@@ -13,6 +15,7 @@ NATS_URI = "nats://nats:nats@127.0.0.1:4222"
 #TODO this might not be the best method, can be changed.
 def get_time():
     return str(int(round(time.time() * 1000)))
+    #return (datetime.datetime.utcnow() - datetime.datetime(2016, 1, 1)).total_seconds()
 
 # Main function that drives the main functionality of the client code.
 #
@@ -43,50 +46,51 @@ def main():
         time.sleep(1)
         print nats.stat.query()
 
-        # subscribe callback function
-        def subscribe_blk(msg):
-            print "[SUB]: received \"{}\"".format(msg)
-            
-            #NOTE: split funciton was not working correctly so the msg string is being 
-            #      stored in a variable
-            #
-            # reveived_message_str= "user_id time_stamp message_number"
-            received_message_str = str(msg)
-            split_message =  received_message_str.split(" ")
-
-            message_number = int(split_message[2])
-            
-            # final_message = "publish_id sent_time my_id received_time"
-            final_message = split_message[0] + " " + split_message[1] + " " + user_id + " " + get_time()
-            received_message_list.append(final_message)
-
-            # determine if the last message for that has been sent
-            if (message_number % 5000) == 0: 
-                #completed_user_list.append(split_message[0])
-                #print "[SUB]: user " + str(user_id) + " finished publishing"
-
-                # check if all of the subcribed users have completed publishing
-                #if completed_user_list == subscribe_list: 
-                    #print "\n[SUB]: all messages received:"
+        if subscribe_list != []: 
+            # subscribe callback function
+            def subscribe_blk(msg):
+                print "[SUB]: received \"{}\"".format(msg)
                 
-                fd = open(filename, "a")
-
-                for i in received_message_list:
-                    print >> fd, "\t" + i
+                #NOTE: split funciton was not working correctly so the msg string is being 
+                #      stored in a variable
+                    #
+                # reveived_message_str= "user_id time_stamp message_number"
+                received_message_str = str(msg)
+                split_message =  received_message_str.split(" ")
+    
+                message_number = int(split_message[2])
+                
+                # final_message = "publish_id sent_time my_id received_time"
+                final_message = split_message[0] + " " + split_message[1] + " " + user_id + " " + get_time()
+                received_message_list.append(final_message)
+    
+                # determine if the last message for that has been sent
+                if (message_number % 5000) == 0: 
+                    #completed_user_list.append(split_message[0])
+                    #print "[SUB]: user " + str(user_id) + " finished publishing"
+    
+                    # check if all of the subcribed users have completed publishing
+                    #if completed_user_list == subscribe_list: 
+                        #print "\n[SUB]: all messages received:"
                     
-                fd.close
-                del received_message_list[:]
-                    
-                if publish_is_complete:
-                    # stop the nats process to exitsthe code cleanly
-                    print "\nStopping NATS process..."
-                    time.sleep(1)    # prevent "[error] Connection closed since ." message
-                    nats.stop()
-
-        # subscribe to every user in the subscribe list
-        for ID in subscribe_list:
-            sid = nats.subscribe(ID, subscribe_blk)
-
+                    fd = open(filename, "a")
+    
+                    for i in received_message_list:
+                        print >> fd, "\t" + i
+                        
+                    fd.close
+                    del received_message_list[:]
+                        
+                    if publish_is_complete:
+                        # stop the nats process to exitsthe code cleanly
+                        print "\nStopping NATS process..."
+                        time.sleep(1)    # prevent "[error] Connection closed since ." message
+                        nats.stop()
+    
+            # subscribe to every user in the subscribe list
+            for ID in subscribe_list:
+                sid = nats.subscribe(ID, subscribe_blk)
+    
         # publish messages and wait between each publish
         for i in range(max_messages):
             nats.publish(user_id, user_id + " " + get_time() + " " + str(i + 1))
