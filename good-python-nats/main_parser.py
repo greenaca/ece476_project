848aeb66db9cfdf69b1ec57a1f0537f67e633f4b
Kai Ovesen <kai.ovesen@gmail.com>
2016-03-04 09:42:02 -0800

    Added a timeout for main parser (currently at 1.5 hrs) and added cron jobs in a text file.

--- good-python-nats/main_parser.py
+++ good-python-nats/main_parser.py
@@ -2,6 +2,34 @@
 
 import subprocess
 import argparse, sys
+from functools import wraps
+import errno
+import os
+import signal
+
+timeout_secs = 54000   # 1.5hrs
+
+class TimeoutError(Exception):
+    pass
+
+def timeout(seconds=10, error_message=os.strerror(errno.ETIME)):
+    def decorator(func):
+        def _handle_timeout(signum, frame):
+            subprocess.call(["pkill", "-9", "python"])
+            raise TimeoutError(error_message)
+
+        def wrapper(*args, **kwargs):
+            signal.signal(signal.SIGALRM, _handle_timeout)
+            signal.alarm(seconds)
+            try:
+                result = func(*args, **kwargs)
+            finally:
+                signal.alarm(0)
+            return result
+
+        return wraps(func)(wrapper)
+
+    return decorator
 
 def show_usage():
   print("main_parser INPUT_FILE [-m MAX_MESSAGES]")
@@ -11,6 +39,7 @@ def show_usage():
   print("main_parser users.txt -m 10000")
 
 
+@timeout(10 + timeout_secs)
 def main():
     parser = argparse.ArgumentParser()
 
